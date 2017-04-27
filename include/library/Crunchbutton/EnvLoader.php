bec46816d9715f81a015a73fdf5e3f20215af708
Devin Smith <github@arzynik.com>
2017-04-26 17:51:57 -0700

    allow arrays in env config

--- include/library/Crunchbutton/EnvLoader.php
+++ include/library/Crunchbutton/EnvLoader.php
@@ -8,35 +8,43 @@ class Crunchbutton_EnvLoader {
 	public static function load() {
 		$map = [
 			'ENCRYPTION_KEY' => 'crypt.key',
-			'PHAXIO_KEY' => 'phaxio.live.key',
-			'PHAXIO_SECRET' => 'phaxio.live.secret',
-			'TWILIO_SID' => 'twilio.live.sid',
-			'TWILIO_TOKEN' => 'twilio.live.token',
-			'TWILIO_HOSTNAME' => 'twilio.live.hostname',
-			'STRIPE_PUBLIC' => 'stripe.live.public',
-			'STRIPE_SECRET' => 'stripe.live.secret'
+			'PHAXIO_KEY' => ['phaxio.live.key', 'phaxio.dev.key'],
+			'PHAXIO_SECRET' => ['phaxio.live.secret', 'phaxio.dev.secret'],
+			'TWILIO_SID' => ['twilio.live.sid', 'twilio.dev.sid'],
+			'TWILIO_TOKEN' => ['twilio.live.token', 'twilio.dev.token'],
+			'TWILIO_HOSTNAME' => ['twilio.live.hostname', 'twilio.dev.hostname'],
+			'STRIPE_PUBLIC' => ['stripe.live.public', 'stripe.dev.public'],
+			'STRIPE_SECRET' => ['stripe.live.secret', 'stripe.dev.secret'],
+			'S3_KEY' => 's3.key',
+			'S3_SECRET' => 's3.secret'
 		];
 		$config = c::config();
 
-		foreach ($map as $key => $value) {
-			$parts = explode('.', $value);
-			$field = $config;
-
-			// dont acidently set root element
-			if (!$parts[0]) {
-				return;
+		foreach ($map as $key => $values) {
+			if (!is_array($values)) {
+				$values = [$values];
 			}
 
-			foreach ($parts as $k => $part) {
-				if ($k == count($parts)-1) {
-					$field->{$part} = $_ENV[$key];
-					break;
+			foreach ($values as $value) {
+				$parts = explode('.', $value);
+				$field = $config;
+
+				// dont acidently set root element
+				if (!$parts[0]) {
+					return;
 				}
 
-				if (!$field->{$part}) {
-					$field->{$part} = new Cana_Model;
+				foreach ($parts as $k => $part) {
+					if ($k == count($parts)-1) {
+						$field->{$part} = $_ENV[$key];
+						break;
+					}
+
+					if (!$field->{$part}) {
+						$field->{$part} = new Cana_Model;
+					}
+					$field = $field->{$part};
 				}
-				$field = $field->{$part};
 			}
 		}
 
